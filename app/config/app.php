04ede40005bf7ef6b5a10f41e237c1ee4d21f651
Jaume991 <jaume.riera.1234@gmail.com>
2015-02-25 13:30:23 +0100

    Dependencies added
    larasponse , validations and searchable

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,9 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Sorskod\Larasponse\LarasponseServiceProvider'
+		'Sorskod\Larasponse\LarasponseServiceProvider',
+		'Laracasts\Validation\ValidationServiceProvider',
+		
 
 	),
 
